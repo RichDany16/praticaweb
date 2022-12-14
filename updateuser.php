c69ccf0dd17485bfc92b726011dfeaa1050746f6
Luis <luislopez1099lg@gmail.com>
2022-12-14 12:49:37 -0700

    tipouser not changin now on update fixed

--- updateuser.php
+++ updateuser.php
@@ -6,12 +6,11 @@ $id = $_POST["id"];
 $name = $_POST["name"];
 $email = $_POST["email"];
 $password = $_POST["password"];
-$tipo = $_POST["tipo"];
 $photo = $_POST["photo"];
 
 
 $db = new Db();
-$db->actualizar("users", "name = '$name', email = '$email', password = '$password', tipo = '$tipo', photo = '$photo'", "id = $id");
+$db->actualizar("users", "name = '$name', password = '$password', photo = '$photo'", "id = $id");
 
 header("Location: ./index.php");
 
